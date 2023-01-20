fc192c91008684e55f6456e8b63c9f501d1d0e58
Felipe Gomez <kaboom300@gmail.com>
2023-01-20 10:17:47 +0100

    configurado frontend para funcionar con API

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -1,16 +1,21 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import jsonapiClient from 'ra-jsonapi-client';
+
 import { UserList } from 'components/react-admin/users';
+import { CustomerList, CustomerEdit, CustomerCreate } from 'components/react-admin/customers';
 import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
 import { MigrationList, MigrationEdit, MigrationCreate  } from 'components/react-admin/migrations';
 
+import CustomerIcon from '@mui/icons-material/SupportAgent';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
 import MigrationIcon from '@mui/icons-material/Storage';
 
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
-const dataProvider = jsonServerProvider('http://encuentro.test/api/records');
+//const dataProvider = jsonServerProvider('http://encuentro.test/api/records');
+const dataProvider = jsonapiClient('http://encuentro.test/api');
 
 const RAdmin = () => (
   <Admin
@@ -18,6 +23,8 @@ const RAdmin = () => (
     dataProvider={dataProvider}
     layout={AdminLayout}
   >
+    <Resource name="customers" 
+      list={CustomerList} icon={CustomerIcon} edit={CustomerEdit} create={CustomerCreate} />
     <Resource name="migrations"
       list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
